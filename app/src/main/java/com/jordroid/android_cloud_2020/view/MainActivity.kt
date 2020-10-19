459bb0ece9f52ef75e7a8960bb88aa6feecf5d4e
Serreau Jordane <jordaneserreau@gmail.com>
2020-10-19 20:43:39 +0200

    implement notification

--- app/src/main/java/com/jordroid/android_cloud_2020/view/MainActivity.kt
+++ app/src/main/java/com/jordroid/android_cloud_2020/view/MainActivity.kt
@@ -4,14 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jordroid.android_cloud_2020.R
+import com.jordroid.android_cloud_2020.architecture.CustomNotificationManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
 
+    private lateinit var mCustomNotificationManager : CustomNotificationManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mCustomNotificationManager = CustomNotificationManager(this)
+        mCustomNotificationManager.createNotificationCompatBuilder()
 
         mainButtonActivityIntent.setOnClickListener { goToActivityIntent() }
         mainButtonRecyclerView.setOnClickListener { goToRecyclerView() }
